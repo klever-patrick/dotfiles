56acea7f026a6dfdbfb61092131ec095aefe5264
Patrick Monteiro <88517431+patrickovm@users.noreply.github.com>
2022-09-09 21:48:21 -0300

    Change jetbrains toolbox installation folder

--- scripts/jetbrains-toolbox.sh
+++ scripts/jetbrains-toolbox.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo mkdir -p /opt/jetbrains
-
 wget --show-progress -qO ./jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
 
-sudo tar -C /opt/jetbrains -xf jetbrains-toolbox.tar.gz
+sudo tar -C /opt/ -xf jetbrains-toolbox.tar.gz
 
 rm -rf ./jetbrains-toolbox.tar.gz
